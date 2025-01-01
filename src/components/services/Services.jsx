9dcf133847b36be5d2e40f28334e6207e62a4ddf
Niranga Lakshan <niranga.l@softcodeit.com>
2025-01-01 20:30:26 +0530

    Added services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,65 @@
 import React from "react";
 import "./service.css";
-
+import { BiCheck } from "react-icons/bi";
 const Services = () => {
-  return <section id="service">Services</section>;
+  const services = [
+    {
+      name: "UI/UX Design",
+      serviceList: [
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+      ],
+    },
+    {
+      name: "UI/UX Design",
+      serviceList: [
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+      ],
+    },
+    {
+      name: "UI/UX Design",
+      serviceList: [
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+        "Lorem, Ipsum dolor sit amet consectetur elit",
+      ],
+    },
+  ];
+
+  return (
+    <section id="service">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        {services.map((services, i) => (
+          <article key={i} className="service">
+            <div className="service__head">
+              <h3>{services.name}</h3>
+            </div>
+
+            <div className="service__list">
+              {services.serviceList.map((item, j) => (
+                <li key={j}>
+                  <BiCheck className="service__list__icon" />
+                  <p>{item}</p>
+                </li>
+              ))}
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Services;
